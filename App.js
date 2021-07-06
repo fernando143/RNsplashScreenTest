297779f4c849fcd407ac042c4d0de3a6f871499f
Fernando Casabuena <azarias08@gmail.com>
2021-07-06 20:22:56 -0300

    configuracion splash screen

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import type {Node} from 'react';
 import {
   SafeAreaView,
@@ -26,8 +26,15 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import SplashScreen from 'react-native-splash-screen'
+
 const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, [])
+
   return (
     <View style={styles.sectionContainer}>
       <Text
